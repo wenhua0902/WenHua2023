5a4a78be8bb58a3405c509ce0507549cb251b58b
wenhua0902 <s1100396@gm.pu.edu.tw>
2023-06-11 16:47:59 +0800

    Initial commit

---
+++ app/src/main/java/tw/edu/pu/gm/s1100396/wenhua2023/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package tw.edu.pu.gm.s1100396.wenhua2023.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
