a13b8ffd137ab74f55609d4fc57b3298aef2090a
wenhua0902 <s1100396@gm.pu.edu.tw>
2023-06-11 17:16:08 +0800

    version 2 Compose元件

--- app/src/main/java/tw/edu/pu/gm/s1100396/wenhua2023/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/gm/s1100396/wenhua2023/MainActivity.kt
@@ -3,12 +3,17 @@ package tw.edu.pu.gm.s1100396.wenhua2023
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.gm.s1100396.wenhua2023.ui.theme.WenHua2023Theme
 
@@ -22,7 +27,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    MapScreen()
                 }
             }
         }
@@ -32,6 +38,23 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+}
+
+
+
+@Composable
+fun MapScreen() {
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        
+        Text(text = "作者:資管二B 黃汶樺")
+        Image(
+            painter = painterResource(id = R.drawable.map),
+            contentDescription = "清水地圖"
+        )
+    }
 }
 
 @Preview(showBackground = true)
